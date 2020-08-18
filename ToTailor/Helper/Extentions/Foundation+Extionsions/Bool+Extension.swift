d474dd4c8e828f8218e20698b54be4f2590f2bf1
Aurangzaib-invozone <muhammad.aurangzaib@invozone.com>
2020-08-18 20:40:51 +0500

    Add new extension for Bool

---
+++ ToTailor/Helper/Extentions/Foundation+Extionsions/Bool+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Bool+Extension.swift
+//  ToTailor
+//
+//  Created by Aurangzaib-invozone on 18/08/2020.
+//  Copyright © 2020 Aurangzaib-invozone. All rights reserved.
+//
+
+import Foundation
